495e6035f44c02a0f14c98f0c68f86f915b0cefe
hiragino-shun <109703971+hiragino-shun@users.noreply.github.com>
2023-12-05 13:28:41 +0900

    Update script.js(2023/12/05/13/28)

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var osaka = new Array(
-  "https://github.com/hiragino-shun/onlymap-beta/blob/a53e348f65630f1597d5e0e3f2e9167198e786e9/osakal.html",
-  "https://github.com/hiragino-shun/onlymap-beta/blob/a53e348f65630f1597d5e0e3f2e9167198e786e9/osakan.html"
+  "osakal.html",
+  "osakan.html"
 );
 function random_jump_o() {
   var i = Math.floor(Math.random() * osaka.length);
@@ -8,8 +8,8 @@ function random_jump_o() {
 }
 
 var saitama = new Array(
-  "https://github.com/hiragino-shun/onlymap-beta/blob/a53e348f65630f1597d5e0e3f2e9167198e786e9/saiatamal.html",
-  "https://github.com/hiragino-shun/onlymap-beta/blob/a53e348f65630f1597d5e0e3f2e9167198e786e9/saitaman.html"
+  "saiatamal.html",
+  "saitaman.html"
 );
 function random_jump_s() {
   var i = Math.floor(Math.random() * saitama.length);
@@ -17,8 +17,8 @@ function random_jump_s() {
 }
 
 var fukuoka = new Array(
-  "https://github.com/hiragino-shun/onlymap-beta/blob/a53e348f65630f1597d5e0e3f2e9167198e786e9/fukuokal.html",
-  "https://github.com/hiragino-shun/onlymap-beta/blob/a53e348f65630f1597d5e0e3f2e9167198e786e9/fukuokan.html"
+  "fukuokal.html",
+  "fukuokan.html"
 );
 function random_jump_f() {
   var i = Math.floor(Math.random() * fukuoka.length);
