8cde843340d55c18782016658f4811ebb673a3eb
hiragino-shun <109703971+hiragino-shun@users.noreply.github.com>
2023-12-13 21:37:44 +0900

    script.js(2023/12/13/2137)
    横浜・京都追加準備

--- script.js
+++ script.js
@@ -16,15 +16,6 @@ function random_jump_s() {
   location.href = saitama[i];
 }
 
-var kobe = new Array(
-  "kobel.html",
-  "koben.html"
-);
-function random_jump_k() {
-  var i = Math.floor(Math.random() * kobe.length);
-  location.href = kobe[i];
-}
-
 var fukuoka = new Array(
   "fukuokal.html",
   "fukuokan.html"
@@ -32,4 +23,22 @@ var fukuoka = new Array(
 function random_jump_f() {
   var i = Math.floor(Math.random() * fukuoka.length);
   location.href = fukuoka[i];
+}
+
+var yokohama = new Array(
+  "yokohamal.html",
+  "yokohaman.html"
+);
+function random_jump_y() {
+  var i = Math.floor(Math.random() * yokohama.length);
+  location.href = yokohama[i];
+}
+
+var kyoto = new Array(
+  "kyotol.html",
+  "kyoton.html"
+);
+function random_jump_m() {
+  var i = Math.floor(Math.random() * kyoto.length);
+  location.href = kyoto[i];
 }
